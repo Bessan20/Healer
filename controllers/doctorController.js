b2a19426446d10343a1e99d94337f31eccc7a4e7
abc <abc@example.com>
2025-02-19 18:01:23 +0200

    doctor controller

---
+++ controllers/doctorController.js
@@ -0,0 +1,77 @@
+const factory = require('./handlersFactory.js');
+const Doctor = require('../models/userModel.js');
+const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const apiError = require('../utils/apiError.js');
+const signToken = (ID) => {
+    return jwt.sign({ ID }, process.env.SECRET_STR, {
+        expiresIn: process.env.LOGIN_EXPIRES,
+    });
+};
+
+const getAllDoctors = factory.getAll(Doctor);
+
+const signUpDoctor = asyncHandler(async (req, res, next) => {
+    const doctor = await Doctor.create(req.body);
+    const token = signToken(doctor._id );
+    res.status(201).json({
+        Status: true,
+        Message: 'Doctor account  created successfully',
+        token,
+        data: { doctor },
+    });
+});
+
+const loginWithIdDoctor = asyncHandler(async(req,res,next)=>{
+
+    const {nationalID , password } = req.body;
+
+    if(!nationalID)
+        return next(new apiError('Please  , enter your national ID.', 400));
+    if(!password)
+        return next(new apiError('Please , enter your account password.', 400));
+
+    const doctor = await Doctor.findOne({nationalID}).select('+password');
+
+    if(!getAllDoctors)
+        return next(new apiError('Invalid national ID.', 400));
+
+    if(!(await doctor.comparePasswordInDb(password , doctor.password)))
+    return next(new apiError('Invalid password.', 400));
+   
+    const token = signToken(doctor._id );
+    res.json({Status : true, Message :"Login successful", token});
+});
+
+const loginWithEmailDoctor = asyncHandler(async(req,res,next)=>{
+
+    const {email , password } = req.body;
+
+    if(!email)
+        return next(new apiError('Please  , enter your email.', 400));
+    if(!password)
+        return next(new apiError('Please , enter your account password.', 400));
+
+    const doctor = await Doctor.findOne({email}).select('+password');
+
+    if(!doctor) 
+        return next(new apiError('Invalid email.', 400));
+    
+    
+    if(!(await doctor.comparePasswordInDb(password , doctor.password)))
+        return next(new apiError('Invalid password.', 400));
+    
+    const token = signToken(doctor._id);
+    res.json({Status : true, Message :"Login successful", token});
+    
+});
+
+module.exports = {
+
+    getAllDoctors,
+    signUpDoctor,
+    loginWithIdDoctor,
+    loginWithEmailDoctor,
+
+  
+};
