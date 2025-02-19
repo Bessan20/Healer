90705104bf55ac323d08ae1971b96da10c1353fb
abc <abc@example.com>
2025-02-19 18:16:32 +0200

    doctor router

---
+++ routes/doctorRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+//*  import controllers 
+const {
+
+    getAllDoctors,
+    signUpDoctor,
+    loginWithIdDoctor,
+    loginWithEmailDoctor,
+} =
+    require('../controllers/authController.js');
+
+//*  import validator
+
+const {
+
+    signUpValidator
+} = require('../utils/validators/doctorValidator.js');
+
+router.route('/getAllDoctors').get(getAllDoctors);
+
+router.route('/signUp').post(signUpValidator, signUpDoctor);
+
+router.route('/loginWithId').post(loginWithIdDoctor);
+
+router.route('/loginWith').post(loginWithEmailDoctor);
+
+module.exports = router;
