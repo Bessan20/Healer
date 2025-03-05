aa63c59ff8f03195a3b90e56bba7cadf156056c3
abc <abc@example.com>
2025-03-05 22:36:55 +0200

    appointmentModel

---
+++ models/appointmentModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+
+    patientName : {
+        
+        type : String,
+        required : true,
+    },
+
+    patientMobile : {
+        
+        type : String,
+        required : true,
+    },
+
+    patientId : {
+
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+    },
+
+    doctorName : {
+        
+        type : String,
+        required : true,
+    },
+
+    doctorId : {
+        
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Doctor',
+    },
+
+    appointmentDate : {
+        
+        type : Date,
+        required : true,
+    },
+
+},{timestamps : true});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
