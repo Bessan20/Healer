98dc89309cc1f172164a4220a1bea62978e9757f
abc <abc@example.com>
2025-03-06 01:50:45 +0200

    appointmentController

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -5,7 +5,15 @@ const apiError = require('../utils/apiError');
 
 const getAllAppointments = factory.getAll(Appointment);
 
+const createAppointment = asyncHandler(async(req,res,next)=>{
+
+    console.log(req.user._id);
+    console.log(req.user.name);
+    res.status(201).send(req.user.name);
+});
+
 module.exports = {
 
-    getAllAppointments
+    getAllAppointments,
+    createAppointment,
 }
