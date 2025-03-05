3c162bbb27759db83f5492c5e7b172d439b69c2c
abc <abc@example.com>
2025-03-06 01:52:55 +0200

    update

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getAllAppointments
+    getAllAppointments,
+    createAppointment
 } = require('../controllers/appointmentController.js');
 
+const { protect } = require('../controllers/authController.js')
 router.route('/getAllAppointments').get(getAllAppointments);
+router.route('/createAppointment').post(protect,createAppointment);
 module.exports = router;
