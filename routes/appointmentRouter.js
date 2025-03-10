ad5fe2ddb60c385ffe28e795bd8977357b602ea2
abc <abc@example.com>
2025-03-10 15:32:14 +0200

    appointmentRoute

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -6,9 +6,9 @@ const {
     cancelAppointment,
 } = require('../controllers/appointmentController.js');
 
-const {createAppointmentValidator} = require('../utils/validators/appointmentValidator.js');
+const {createAppointmentValidator , cancelAppointmentValidator} = require('../utils/validators/appointmentValidator.js');
 const { protect } = require('../controllers/authController.js')
 router.route('/getAllAppointments').get(getAllAppointments);
 router.route('/createAppointment').post(protect,createAppointmentValidator,createAppointment);
-router.route('/cancelAppointment').delete(protect,cancelAppointment);
+router.route('/cancelAppointment').delete(protect,cancelAppointmentValidator,cancelAppointment);
 module.exports = router;
