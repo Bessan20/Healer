f39482138b7639051337e8aa59eeb9025890701c
Mohamed Samir <mohamedsamir2592012@gmail.com>
2025-03-30 15:30:05 +0200

    Merge resolved: No changes in index.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-
-const dbConnection = ()=>{
-
-    mongoose.connect(process.env.DB_URI)
-    .then((conn)=>{
+const dbConnection = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_URI);
         console.log(`Connected to MongoDB successfully: ${conn.connection.host}`);
-    })
-    
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1); // Exit the process if connection fails
+    }
 };
 
 module.exports = dbConnection;
--- index.js
+++ index.js
@@ -61,3 +61,4 @@ process.on("unhandledRejection",(err)=>{
 });
 
 
+module.exports = app;
