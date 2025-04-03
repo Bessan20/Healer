c65ef169246bf42d7c7dba5fd8b724ac2b490ac9
abc <abc@example.com>
2025-04-03 16:12:13 +0200

    newChanges

--- controllers/socialController.js
+++ controllers/socialController.js
@@ -4,27 +4,25 @@ const factory = require("./handlerFactory.js");
 const apiError = require("../utils/apiError.js");
 const multer = require("multer");
 
+const getAllSocial = factory.getAll(Social);
+
 const createSocial = asyncHandler(async (req, res, next) => {
-    const { photo, fullName, email, phone, married, children, job } = req.body;
     
-    if (!photo || !fullName || !email || !phone || !married || !children || !job) {
-        return next(new apiError("Please fill in all fields.", 400));
-    }
-    
-    const social = await Social.create({
-        photo,
-        fullName,
-        email,
-        phone,
-        married,
-        children,
-        job,
-    });
     
     res.status(201).json({
         Status: true,
         Message: "Social information created successfully",
-        data: { social },
+       
     });
     }
 );
+
+module.exports = {
+
+    getAllSocial,
+    createSocial
+
+
+}
+
+
