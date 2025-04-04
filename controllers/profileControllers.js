56ac1932e406eaaf8cfef35266f63c681f9c9977
abc <abc@example.com>
2025-04-05 00:01:02 +0200

    new

--- controllers/profileControllers.js
+++ controllers/profileControllers.js
@@ -43,33 +43,30 @@ const updateProfile = asyncHandler(async(req,res,next)=>{
 
     const userId = req.user._id;
 
-
-    //if profile not exist create new one
-    const profile = await Profile.findOne({userId});
-    
-    if(!profile) {
-
-       const newProfile = await Profile.create({
-            fullName : req.user.name,
-            nationalID : req.user.nationalID,
-            email : req.user.email,
-            phone : req.user.mobilePhone,
-            userId : req.user._id,
+    let profile = await Profile.findOne({ userId });
+    if (!profile) {
+        profile = await Profile.create({
+            fullName: req.user.name,
+            nationalID: req.user.nationalID,
+            email: req.user.email,
+            phone: req.user.mobilePhone,
+            userId: req.user._id,
         });
-        
+        return res.status(201).json({
+            success: true,
+            message: "Profile created successfully",
+            data: {
+                profile,
+            },
+        });
     }
 
-    //find profile id
-    const profileId = await Profile.findOne({userId});
-    //if profile exist update it
-    const updatedProfile = await Profile.findByIdAndUpdate(profileId._id, req.body, {
+    const updatedProfile = await Profile.findByIdAndUpdate(profile._id, req.body, {
         new: true,
         runValidators: true,
     });
 
-    //update user data
-
-    const updatedUser = await User.findByIdAndUpdate(req.user._id,{
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, {
         name: req.body.fullName || req.user.name,
         nationalID: req.body.nationalID || req.user.nationalID,
         mobilePhone: req.body.phone || req.user.mobilePhone,
@@ -82,7 +79,7 @@ const updateProfile = asyncHandler(async(req,res,next)=>{
     res.status(200).json({
         success: true,
         message: "Profile updated successfully",
-        data : {
+        data: {
             profile: updatedProfile,
             user: updatedUser,
         },
