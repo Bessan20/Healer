f0da68f025d21bd9b6fe2ae526c6fb3ca3854d44
abc <abc@example.com>
2025-04-04 17:12:38 +0200

    socialValidator

--- utils/validators/socialValidator.js
+++ utils/validators/socialValidator.js
@@ -12,10 +12,42 @@ const validatorMiddleware = require('../../middlewares/validatorMiddleware.js');
 
 const signUpValidator = [
 
-    check('fullName')
+    check('image')
     .notEmpty()
-    .withMessage('name is required.'),
-    validatorMiddleware
+    .withMessage('Your national id image is required.')
+    ,check('fullName')
+    .notEmpty()
+    .withMessage('Your full name is required.')
+    .trim()
+    ,check('email')
+    .notEmpty()
+    .withMessage('Your email is required.')
+    .isEmail()
+    .withMessage('Your email is invalid.')
+    .trim()
+    ,check('phone')
+    .notEmpty()
+    .withMessage('Your phone number is required.')
+    //phone number must be 11 digits
+    .custom((phone)=> {
+        const regex = /^[0-9]{11}$/;
+        if (!regex.test(phone)) {
+          throw new Error('Invalid mobile phone number , it should be 11 digits.');
+        }
+        return true;
+      })
+    .trim()
+    ,check('married')
+    .notEmpty()
+    .withMessage('Your marital status is required.')
+    ,check('children')
+    .notEmpty()
+    .withMessage('Your children status is required.')
+    ,check('job')
+    .notEmpty()
+    .withMessage('Your job title status is required.')
+    .trim()
+    ,validatorMiddleware
 ];
 
 module.exports = {
