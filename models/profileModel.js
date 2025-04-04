b862760d2b8cc62de4c9c81870cbb260594a3389
abc <abc@example.com>
2025-04-04 22:05:51 +0200

    profileModel

---
+++ models/profileModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+
+    image : {
+        type: String,
+        
+    },
+    fullName: {
+        type: String,
+        
+    },
+    nationalID: {
+        type: String,
+        
+    },
+    email: {
+        type: String,
+        
+    },
+    phone: {
+        type: String,
+        
+    },
+    date: {
+        type: Date,
+        
+    },
+    gender: {
+        type: String,
+        
+    },
+    blood : {
+        type: String,
+        
+    },
+    notes : {
+        type: String,
+        
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+}, { timestamps: true });
