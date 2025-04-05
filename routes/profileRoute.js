4699383454d3befda0a13a3eb09cfe16b6d4c029
abc <abc@example.com>
2025-04-05 21:33:55 +0200

    addUploadFile

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../controllers/authController.js');
-const {userProfile , getProfile , updateProfile} = require('../controllers/profileControllers.js');
+const {userProfile , getProfile , updateProfile , uploadFile} = require('../controllers/profileControllers.js');
 //const {userProfileValidator} = require('../utils/validators/profileValidator.js');
 router.route('/userProfile').get(protect, userProfile);
 
 router.route('/getProfile').get(protect, getProfile);
 
-router.route('/updateProfile').patch(protect,updateProfile);
+router.route('/updateProfile').patch(protect,uploadFile,updateProfile);
 
 module.exports = router;
