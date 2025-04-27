a51c54b72a81f0635a19630bac2941a61e8380ce
abc <abc@example.com>
2025-04-27 12:07:54 +0300

    profileRoute

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../controllers/authController.js');
-const {userProfile , getProfile , updateProfile , uploadFile} = require('../controllers/profileControllers.js');
+const {userProfile , getProfile , updateProfile , uploadFile , updateImageProfile} = require('../controllers/profileControllers.js');
 //const {userProfileValidator} = require('../utils/validators/profileValidator.js');
 router.route('/userProfile').get(protect, userProfile);
 
 router.route('/getProfile').get(protect, getProfile);
 
 router.route('/updateProfile').patch(protect,uploadFile,updateProfile);
+
+router.route('/updateImageProfile').patch(protect,uploadFile,updateImageProfile);
 
 module.exports = router;
