b53f15be6f38d2608a3ec2b0912d86d41b6b7de7
abc <abc@example.com>
2025-04-27 12:02:59 +0300

    profileController

--- controllers/profileControllers.js
+++ controllers/profileControllers.js
@@ -107,10 +107,72 @@ const updateProfile = asyncHandler(async(req,res,next)=>{
 
 
 });
+
+const updateImageProfile = asyncHandler(async(req,res,next)=>{
+
+    const userId = req.user._id;
+
+    let profile = await Profile.findOne({ userId });
+    if (!profile) {
+        profile = await Profile.create({
+            fullName: req.user.name,
+            nationalID: req.user.nationalID,
+            email: req.user.email,
+            phone: req.user.mobilePhone,
+            userId: req.user._id,
+        });
+        
+    }
+
+    let imageUrl = ''; 
+    // If an image is provided, upload it to Cloudinary or process it
+    if (req.file) {
+        const result = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+                { folder: 'uploads' },
+                (error, result) => {
+                    if (error) {
+                        reject(new Error('Failed to upload image to Cloudinary.'));
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+            stream.end(req.file.buffer);
+        });
+
+       imageUrl = result.secure_url; // Store the uploaded image URL
+    }
+    
+    
+    const updatedProfile = await Profile.findByIdAndUpdate(profile._id, 
+        {
+        
+      ...(imageUrl && { image: imageUrl }), // Only update the image if it exists
+    }, {
+        new: true,
+        runValidators: true,
+    });
+
+    
+    console.log(profile._id);
+    console.log(req.body);
+    res.status(200).json({
+        success: true,
+        message: "Profile updated successfully",
+        data: {
+            updatedProfile,
+            
+        }
+    });
+
+
+});
 module.exports = {
 
     userProfile,
     getProfile,
     updateProfile,
     uploadFile,
+    updateImageProfile,
 }
