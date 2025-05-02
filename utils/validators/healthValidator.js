9fac3790fddbdb5b16b82b052af85f8e7b902fe8
abc <abc@example.com>
2025-05-02 14:44:43 +0300

    healthValidator

---
+++ utils/validators/healthValidator.js
@@ -0,0 +1,15 @@
+const {check , body} = require('express-validator');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware.js');
+
+const Health = require('../../models/healthModel.js');
+
+const createHealthValidator = [ 
+
+
+    ,validatorMiddleware]
+
+module.exports = { 
+
+    createHealthValidator
+    
+};
