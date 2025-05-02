f8edb9e2ce32f1e607786ece15c17afa7f138f44
abc <abc@example.com>
2025-05-02 12:51:53 +0300

    healthModel

---
+++ models/healthModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const healthSchema = new mongoose.Schema({
+
+    headFamily : {
+        
+        type : String,
+        required : [true, 'Please enter the head of the family name.'],
+        trim : true,
+    },
+    beneficiaryName : {
+        
+        type : String,
+        required : [true, 'Please enter the beneficiary name.'],
+        trim : true,
+    },
+    healthUnit : {
+        
+        type : String,
+        required : [true, 'Please enter the health unit name.'],
+        trim : true,
+    },
+    fileNumber : {
+        
+        type : String,
+        required : [true, 'Please enter the file number.'],
+        unique : [true , 'File number already exists.'],
+        trim : true,
+    },
+
+} , {
+    timestamps: true });
+
+const Health = mongoose.model('Health', healthSchema);
+module.exports = Health;
