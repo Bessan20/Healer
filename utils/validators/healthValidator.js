c5608dfd88efe29b4a4e50f40d42f0680002efdb
abc <abc@example.com>
2025-05-02 14:58:28 +0300

    new

--- utils/validators/healthValidator.js
+++ utils/validators/healthValidator.js
@@ -9,6 +9,28 @@ const createHealthValidator = [
     .notEmpty()
     .withMessage('Head of the family name is required.')
     .trim()
+    ,check('beneficiaryName')
+    .notEmpty()
+    .withMessage('Beneficiary name is required.')
+    .trim()
+    ,check('healthUnit')
+    .notEmpty()
+    .withMessage('Health unit is required.')
+    .trim()
+    ,check('fileNumber')
+    .notEmpty()
+    .withMessage('File number is required.')
+    .trim()
+    //should be unique
+    .custom((val)=>
+    
+        Health.findOne({fileNumber: val}).then((health)=>
+            {
+                if(health){
+                    return Promise.reject(new Error('File number already exists.'))
+                }
+            }
+        ))
 
     ,validatorMiddleware]
 
