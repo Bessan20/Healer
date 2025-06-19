d83d51bf548a5c98325b7333eedca90f77476eff
mo7amednabih <eid46060@gmail.com>
2025-06-19 16:37:16 +0300

    update add medicine

--- routes/medicineRouter.js
+++ routes/medicineRouter.js
@@ -19,7 +19,7 @@ router.route("/getMyMedicine").get(protect, getMyMedicines);
 router.route("/createMedicne").post(protectDoctor, createMedicines);
 
 // Route to cancel an appointment
-router.route("/updateStartTime").delete(protect, adjustMedicineTime);
+router.route("/updateStartTime").put(protect, adjustMedicineTime);
 
 // Export the router
 module.exports = router;
