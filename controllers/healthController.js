853b8a5d3f7e30729f7b0857ff71dcae8c860062
abc <abc@example.com>
2025-06-21 00:17:59 +0300

    new updates

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -25,9 +25,24 @@ const createHealthInsurance = asyncHandler(async (req, res, next) => {
     });
 });
 
+const getHealthInsuranceByUser = asyncHandler(async (req, res, next) => {
+    const userId = req.user._id; // Assuming you have the user ID in req.user
+    const healthInsurances = await Health.find({ userId }).select('-__v -userId -_id');
+    if (!healthInsurances || healthInsurances.length === 0) {
+        return next(new apiError('No health insurance found for this user', 404));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            healthInsurances
+        }
+    });
+});
+
 module.exports = {
     
     getHealthInsurance,
-    createHealthInsurance
+    createHealthInsurance,
+    getHealthInsuranceByUser
 
 };
--- routes/healthRouter.js
+++ routes/healthRouter.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {
     getHealthInsurance,
-    createHealthInsurance } = require('../controllers/healthController.js');
+    createHealthInsurance ,getHealthInsuranceByUser} = require('../controllers/healthController.js');
 const {createHealthValidator} = require('../utils/validators/healthValidator.js');
     const { protect } = require('../controllers/authController.js');
 
 router.route('/getHealthInsurance').get(getHealthInsurance);
 
 router.route('/createHealthInsurance').post(protect, createHealthValidator,createHealthInsurance);
+
+router.route('/getHealthInsuranceByUser').get(protect, getHealthInsuranceByUser);
 
 module.exports = router;
