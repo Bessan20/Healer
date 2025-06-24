77bb0eb7221ed1676a7c4a576b0f3f88843e1f18
abc <abc@example.com>
2025-06-24 13:34:24 +0300

    new

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -5,38 +5,32 @@ const reviewSchema = new mongoose.Schema({
     userId : {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true,
         trim: true,
     },
 
     userName : {
         type: String,
-        required: true,
         trim: true,
     },
 
     userImage : {
         type: String,
-        required: true,
         trim: true,
     },
 
     doctorId : {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Doctor',
-        required: true,
         trim: true,
     },
 
     reviewText : {
         type: String,
-        required: true,
         trim: true,
     },
 
     rating : {
         type: Number,
-        required: true,
         min: 1,
         max: 5,
         
